3a5e58dc9dee8dc300d7aa9b321c5c411a9080d6
nikolasg22 <nikolasgv@gmail.com>
2015-11-13 14:42:33 -0300

    Update index.php

--- proyecto1/views/album/index.php
+++ proyecto1/views/album/index.php
@@ -13,7 +13,7 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="album-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+    <?php 
 
     <p>
         <?= Html::a('Create Album', ['create'], ['class' => 'btn btn-success']) ?>
